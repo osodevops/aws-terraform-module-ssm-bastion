8240239e059c1ddcb5dc5ea74b90a089909aab92
Sion Smith <sion.smith@gmail.com>
2019-01-13 17:59:44 +0000

    removed SSM agent from userdata as this is now in hardened AMI build

--- files/userdata.sh
+++ files/userdata.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-cd /tmp
-yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-systemctl start amazon-ssm-agent
-yum install -y epel-release
-yum install -y python-pip && pip install pip -U && pip install ansible -U
+# commands which you want to run on startup
